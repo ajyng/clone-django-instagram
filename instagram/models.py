992a6bb010876cad026fdf4ab1cf4e10275a0aed
ahnjuny0ung <67837091+ahnjuny0ung@users.noreply.github.com>
2020-11-07 19:47:13 +0900

    timeline view

--- instagram/models.py
+++ instagram/models.py
@@ -3,12 +3,20 @@ from django.conf import settings
 import re
 from django.urls import reverse
 
-class Post(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True # 실제 DB table은 생성되지 않는다.
+
+class Post(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500)
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100)
+    
 
     def __str__(self):
         return self.caption
--- instagram/views.py
+++ instagram/views.py
@@ -3,15 +3,29 @@ from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from django.contrib import messages
 from .models import Tag, Post
+from django.db.models import Q
 from django.contrib.auth import get_user_model
+from django.utils import timezone
+from datetime import timedelta
 
 @login_required
 def index(request):
+    timesince = timezone.now() - timedelta(days=3)
+    
+    post_list = Post.objects.all().filter(
+            Q(author__in = request.user.following_set.all()) |
+            Q(author = request.user)
+        )\
+        .filter(
+            created_at__gte=timesince
+        )
+
     suggested_user_list = get_user_model().objects.all()\
         .exclude(pk=request.user.pk)\
         .exclude(pk__in=request.user.following_set.all())[:3]
 
     return render(request, "instagram/index.html", {
+        "post_list":post_list,
         "suggested_user_list":suggested_user_list,
     })
 
@@ -44,9 +58,15 @@ def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
     post_list_count = post_list.count() # 실제 DB에 count 쿼리를 던지게 된다.
+
+    if request.user.is_authenticated:
+        is_follow = request.user.following_set.filter(pk=page_user.pk).exists()
+    else:
+        is_follow = False
     
     return render(request, "instagram/user_page.html", {
         "page_user": page_user,
         "post_list": post_list,
         "post_list_count": post_list_count,
+        "is_follow": is_follow,
     })
