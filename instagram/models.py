424001ff640eec5b6cb79cc40fd1e53811a98a61
ahnjuny0ung <67837091+ahnjuny0ung@users.noreply.github.com>
2020-11-08 03:37:16 +0900

    포스팅 좋아요-취소 구현

--- instagram/models.py
+++ instagram/models.py
@@ -10,12 +10,19 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True # 실제 DB table은 생성되지 않는다.
 
+
+# 특정 유저의 post를 불러올 때
+#   -> Post.objects.all()
+#   -> user.post_set.all()
+# 2가지의 방법을 사용한다.
+
 class Post(BaseModel):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='my_post_set', on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500)
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100)
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_post_set', blank=True)
     
 
     def __str__(self):
@@ -31,6 +38,12 @@ class Post(BaseModel):
             tag, _ = Tag.objects.get_or_create(name=tag_name)
             tag_list.append(tag)
         return tag_list
+
+    def is_like_user(self, user):
+        return self.like_user_set.filter(pk=user.pk).exists()
+
+    class Meta:
+        ordering = ['-id']
     
 
 class Tag(models.Model):
@@ -38,3 +51,7 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+# class LikeUser(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
